74eb76e9a1bff8adfce50ed171946fc7715b8bb4
Sam <springsang_dev@163.com>
2021-08-06 22:29:18 +0800

    first commit

---
+++ src/main/java/cn/edu/ccut/git/GitTest.java
@@ -0,0 +1,11 @@
+package cn.edu.ccut.git;
+
+/**
+ * @author springsang
+ * @create 2021-08-06 21:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
